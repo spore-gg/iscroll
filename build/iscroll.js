be4973786cc0ae4402d1b4b74507528d474da681
Austin Hallock <austin@clay.io>
2016-12-16 15:55:38 -0800

    temporary fix for broken iscroll in chrome 55 https://github.com/cubiq/iscroll/issues/1109#issuecomment-267661774

--- build/iscroll.js
+++ build/iscroll.js
@@ -294,9 +294,9 @@ function IScroll (el, options) {
 		snapThreshold: 0.334,
 
 // INSERT POINT: OPTIONS
-		disablePointer : !utils.hasPointer,
-		disableTouch : utils.hasPointer || !utils.hasTouch,
-		disableMouse : utils.hasPointer || utils.hasTouch,
+		disablePointer : true,
+		disableTouch : false,
+		disableMouse : true,
 		startX: 0,
 		startY: 0,
 		scrollY: true,
